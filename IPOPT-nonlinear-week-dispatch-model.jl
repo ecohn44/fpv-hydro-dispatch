2a376f8298f9aa5cff7bff758ae449dadb00f8e5
Eliza Cohn <54812269+ecohn44@users.noreply.github.com>
2024-06-12 13:26:40 -0400

    finished ipopt sims

--- IPOPT-nonlinear-week-dispatch-model.jl
+++ IPOPT-nonlinear-week-dispatch-model.jl
@@ -8,7 +8,7 @@ using Dates
 using Plots
 using Base.Filesystem
 using Ipopt
-
+include("plots.jl")
 
 # ----------------- UNIT CONVERSION ----------------- #
 
@@ -78,32 +78,28 @@ storage_2d = [af_to_m3(row["total storage (ac-ft)"]) for row in eachrow(storage)
 hpcap = DataFrame(CSV.File(hpcap_path)).Column1;
 
 
-# ----------------- REDUCTION FACTOR ----------------- #
-# beta = .5
-
 # ----------------- SIMULATION PARAMETERS ----------------- #
 ## Overview: Power contractors operate on weekly subcontracts. 
 # Therefore, will let the simulation run every hour of the week before resetting for the next water contract 
 
 T = 24; # time step per day
-N_sim = 1; # days per week
-W_sim = 1; # number of weeks 
-s2hr = 3600  # seconds in an hour (delta T)
+N = 7; # days per week
+s2hr = 3600  # seconds in an hour (delta t)
 
 # Extract data for time period of interest
-L = RTP_2d[1:T*N_sim,1];    # hourly LMP [$/MWh]
-Q = netflow_2d[1:N_sim,1];  # daily system net flow into [m3]
-U = release_2d[1:N_sim,1]   # daily total release contract [m3]
-S = storage_2d[1:N_sim,1]   # historical daily storage levels [m3]
+L = RTP_2d[1:T*N,1];    # hourly LMP [$/MWh]
+Q = netflow_2d[1:N,1];  # daily system net flow into [m3]
+U = release_2d[1:N,1]   # daily total release contract [m3]
+S = storage_2d[1:N,1]   # historical daily storage levels [m3]
 q = dailyflow_to_hourly(Q, T); # hourly system net flow (constant over each day) [m3]
-alpha_norm_w = repeat(alpha_norm, N_sim) # replicate available solar capacity for every day of the sim. 
-alpha = alpha_norm_w[1:T*N_sim]
+alpha_norm_w = repeat(alpha_norm, N) # replicate available solar capacity for every day of the sim. 
+alpha = alpha_norm_w[1:T*N]
 
 V0 = S[1]   # initial reservoir conditions [~1.9 e10 m3]
 e = 0    # evaporation constant [m/day]
 min_ut = cfs_to_m3s(5000)    # min daily release limit [m3/s]
 max_ut = cfs_to_m3s(25000)   # max daily release limit [m3/s] 
-min_Vt = S0 - T*N_sim*s2hr*max_ut # min reservoir levels
+min_Vt = V0 - T*N*s2hr*max_ut # min reservoir levels
 RR_dn = cfs_to_m3s(-2500) # down ramp rate limit [m3/s]
 RR_up = cfs_to_m3s(4000)  # up ramp rate limit [m3/s]
 PF = 3300   # max feeder capacity [MW] (3.3 GW) 
@@ -113,7 +109,7 @@ eta = .9     # efficiency of release-energy conversion
 rho_w = 1000 # density of water [kg/m^3]
 g = 9.8      # acceleration due to gravity [m/s^2]
 
-Uw = sum(U[1:N_sim]); # Weekly Water contract
+Uw = sum(U[1:N]); # Weekly Water contract
 
 # ------------ HYDRAULIC HEAD VARS ------------ #
 a = 15;
@@ -125,35 +121,72 @@ b = 0.13;
 model = Model(Ipopt.Optimizer)
 
 # Define variables
-@variable(model, V[1:T])
+@variable(model, V[1:T*N])
 set_lower_bound.(V, min_Vt)
-@variable(model, p_h[1:T] >= 0)
-@variable(model, p_s[1:T] >= 0)
-@variable(model, u[1:T])
+@variable(model, p_h[1:T*N] >= 0)
+@variable(model, p_s[1:T*N] >= 0)
+@variable(model, u[1:T*N])
 set_upper_bound.(u, max_ut)
 set_lower_bound.(u, min_ut)
 
 # Initial conditions
-@constraint(model, V[1] == V0)
+@constraint(model, MassBalInit, V[1] == V0)
 
 # Objective function
 @objective(model, Max, sum(L .* (p_h + p_s)))
 
 # Constraints
-@constraint(model, [t in 2:T], V[t] == V[t-1] + s2hr*(q[t] - u[t]))
-@constraint(model, s2hr*sum(u) == Uw)
-@constraint(model, [t in 1:T], p_h[t] <= (eta * g * rho_w * u[t] * a * (V[t]^b))/1e6)
-@constraint(model, [t in 1:T], min_ut <= u[t] <= max_ut)
-@constraint(model, [t in 2:T], RR_dn <= u[t] - u[t-1] <= RR_up)
-@constraint(model, [t in 1:T], 0 <= p_s[t] <= alpha[t]*PS)
-@constraint(model, [t in 1:T], 0 <= p_s[t] + p_h[t] <= PF)
+@constraint(model, MassBal[t in 2:T*N], V[t] == V[t-1] + s2hr*(q[t] - u[t]))
+@constraint(model, WaterContract, s2hr*sum(u) >= Uw)
+@constraint(model, ReleaseEnergy[t in 1:T*N], p_h[t] <= (eta * g * rho_w * u[t] * a * (V[t]^b))/1e6)
+@constraint(model, Release[t in 1:T*N], min_ut <= u[t] <= max_ut)
+@constraint(model, RampRate[t in 2:T*N], RR_dn <= u[t] - u[t-1] <= RR_up)
+@constraint(model, SolarCap[t in 1:T*N], 0 <= p_s[t] <= alpha[t]*PS)
+@constraint(model, FeederCap[t in 1:T*N], 0 <= p_s[t] + p_h[t] <= PF)
 
 # Solve the optimization problem
 optimize!(model)
 
+obj = objective_value(model);
+
 # Print the results
-println("Optimal V: ", value.(V))
-println("Optimal p_h: ", value.(p_h))
-println("Optimal p_s: ", value.(p_s))
-println("Optimal u: ", value.(u))
-println("Optimal objective value: ", objective_value(model))
+@printf("Total Profit: \$ %d \n", obj)
+@printf("Total FPV Profit: \$ %d \n", sum(L.*value.(p_s)))
+@printf("Total Hydropower Profit: \$ %d \n \n", sum(L.*value.(p_h)))
+
+@printf("Weekly Water Contract: %d m^3 \n \n", Uw)
+@printf("Simulated Total Water Release: %d m^3 \n \n", sum(value.(u))*s2hr)
+
+# ---------- PLOTS -------- # 
+
+# Create directory for this run 
+stamp = Dates.format(now(), "mm-dd-yyyy HH.MM.SS") ;
+dir = "./plots/" ;
+path = dir * stamp * " IPOPT";
+mkdir(path)
+
+# Generation Plots
+hpcap = (eta * rho_w *g * a * value.(u) .* (value.(V).^b))/1e6 
+gen_plots(path, T*N, value.(p_s), PS, alpha_norm_w, value.(p_h), hpcap, PF)
+
+# Water release
+release_plots(path, T*N, value.(u), min_ut, max_ut)
+
+# Water release overlaid with electicity price
+release_plots_LMP(path, T*N, value.(u), min_ut, max_ut, L)
+
+#println("Optimal V: ", value.(V))
+#println("Optimal p_h: ", value.(p_h))
+#println("Optimal p_s: ", value.(p_s))
+#println("Optimal u: ", value.(u))
+
+# ---------- DUAL VALUES -------- # 
+println("Dual Values")
+println("Water Contract: ", dual.(WaterContract))
+
+#println("Mass Balance: ", dual.(MassBal))
+#println("Release Energy: ", dual.(ReleaseEnergy))
+#println("Water Release: ", dual.(Release))
+#println("Ramp Rate: ", dual.(RampRate))
+#println("Solar Capacity: ", dual.(SolarCap))
+#println("Feeder Capacity: ", dual.(FeederCap))
